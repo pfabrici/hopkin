782b560e719de6378238db5f243345564543abaf
Peter Fabricius <info@peter-fabricius.de>
2024-04-11 16:19:42 +0200

    changes to the datamodel, now job_name and job_folder are PK to a job

--- src/init/ddl/hopkin_ddl.sql
+++ src/init/ddl/hopkin_ddl.sql
@@ -5,100 +5,77 @@ grant all on hopkin.* to dwhetl@'localhost' identified by 'password';
 
 use hopkin;
 
-CREATE TABLE job_def
-(
-   job_name varchar(128) NOT NULL,
-   job_type varchar(255) DEFAULT NULL,
-   script_name varchar(128) DEFAULT NULL,
-   script_type varchar(10) DEFAULT NULL,
-   unique_run varchar(1) DEFAULT 'Y' NOT NULL,
-   deltajob varchar(1) DEFAULT 'N' NOT NULL,
-   deltascript varchar(128) DEFAULT NULL,
-   valid_from datetime DEFAULT current_timestamp() NOT NULL,
-   valid_to datetime DEFAULT '3000-12-31 00:00:00',
-   ins_timestamp datetime DEFAULT current_timestamp(),
-   mod_timestamp datetime DEFAULT current_timestamp(),
-   PRIMARY KEY (job_name,valid_from)
-)
-;
+/*
+DROP TABLE job_param;
+DROP TABLE job_param_group;
+DROP TABLE job_prot;
+DROP TABLE job_delta;
+DROP TABLE job_def;
+*/
+
+
+CREATE TABLE job_def (
+   job_name 		varchar(128) NOT NULL,
+   job_folder 		varchar(255) NOT NULL,
+   script_name 		varchar(128) DEFAULT NULL,
+   script_type 		varchar(10) DEFAULT NULL,
+   is_unique 		varchar(1) DEFAULT 'Y' NOT NULL,
+   is_delta_job 	varchar(1) DEFAULT 'N' NOT NULL,
+   delta_script 	varchar(128) DEFAULT NULL,
+   valid_from 		date DEFAULT current_date(),
+   valid_to 		date DEFAULT '3000-01-01',
+   ins_timestamp 	datetime DEFAULT current_timestamp(),
+   mod_timestamp 	datetime DEFAULT current_timestamp(),
+   PRIMARY KEY (job_name, job_folder, valid_from)
+);
 
 CREATE TABLE job_delta
 (
-   target_name 	varchar(255) NOT NULL,
-   source_name 	varchar(255) NOT NULL,
-   delta_type 	varchar(255) NOT NULL,
-   delta_value 	varchar(255) ,
-   ins_timestamp datetime DEFAULT current_timestamp() NOT NULL,
-   mod_timestamp datetime DEFAULT current_timestamp(),
-   process_id varchar(40),
+   target_name 	        varchar(255) NOT NULL,
+   source_name 	        varchar(255) NOT NULL,
+   delta_type 	        varchar(255) NOT NULL,
+   delta_value 	        varchar(255) ,
+   ins_timestamp        datetime DEFAULT current_timestamp(),
+   mod_timestamp        datetime DEFAULT current_timestamp(),
+   process_id           varchar(40),
    PRIMARY KEY (target_name,source_name,delta_type)
 );
 
 
 CREATE TABLE job_param_group (
-   job_name varchar(128) NOT NULL,
-   param_group varchar(128) NOT NULL,
-   ins_timestamp datetime DEFAULT current_timestamp(),
-   mod_timestamp datetime DEFAULT current_timestamp(),
+   job_name             varchar(128) NOT NULL,
+   job_folder           varchar(128) NOT NULL,
+   param_group          varchar(128) NOT NULL,
+   ins_timestamp        datetime DEFAULT current_timestamp(),
+   mod_timestamp        datetime DEFAULT current_timestamp(),
    PRIMARY KEY (job_name,param_group)
 );
 
-ALTER TABLE job_param_group
-ADD CONSTRAINT job_param_group_ibfk_1
-FOREIGN KEY (job_name)
-REFERENCES job_def(job_name)
-;
-
 CREATE TABLE job_param
 (
-   param_key varchar(128) NOT NULL,
-   param_value varchar(256) ,
-   param_group varchar(128) NOT NULL,
-   param_ispwd varchar(1) DEFAULT 'N' NOT NULL,
-   valid_from datetime DEFAULT '1900-01-01 00:00:00' NOT NULL,
-   valid_to datetime DEFAULT '3000-12-31 23:59:59',
-   mod_timestamp datetime DEFAULT current_timestamp(),
-   configured varchar(20) DEFAULT 'N/A',
+   param_key            varchar(128) NOT NULL,
+   param_value          varchar(256),
+   param_group          varchar(128) NOT NULL,
+   param_type           varchar(128) NOT NULL DEFAULT 'String',
+   is_password          varchar(1) DEFAULT 'N' NOT NULL,
+   configured_by        varchar(20) DEFAULT 'N/A',
+   valid_from           date DEFAULT '1900-01-01' NOT NULL,
+   valid_to             date DEFAULT '3000-12-31' NOT NULL,
+   ins_timestamp        datetime DEFAULT current_timestamp(),
+   mod_timestamp        datetime DEFAULT current_timestamp(),
    PRIMARY KEY (param_key,param_group,valid_from)
-)
-;
+);
 
-DROP TABLE job_prot;
+
 CREATE TABLE job_prot (
-   process_id varchar(40) PRIMARY KEY NOT NULL,
-   job_name varchar(128) NOT NULL,
-   job_status varchar(20) ,
-   os_process_id integer ,
-   duration 	int default 0,
-   ins_timestamp datetime DEFAULT current_timestamp(),
-   mod_timestamp datetime DEFAULT current_timestamp()
-)
-;
-
-ALTER TABLE job_prot
-ADD CONSTRAINT job_prot_ibfk_1
-FOREIGN KEY (job_name)
-REFERENCES job_def(job_name)
-;
-CREATE INDEX job_name ON job_prot(job_name)
-;
-CREATE TABLE job_suffix
-(
-   job_name varchar(128) NOT NULL,
-   source_name varchar(20) DEFAULT NULL,
-   script_suffix varchar(20) DEFAULT NULL,
-   valid_from datetime DEFAULT current_timestamp(),
-   valid_to datetime DEFAULT '3000-12-31 23:59:59',
-   ins_timestamp datetime DEFAULT current_timestamp() NOT NULL,
-   mod_timestamp datetime DEFAULT current_timestamp(),
-   configured varchar(20) DEFAULT 'N/A'
-)
-;
-ALTER TABLE job_suffix
-ADD CONSTRAINT job_suffix_ibfk_1
-FOREIGN KEY (job_name)
-REFERENCES job_def(job_name)
-;
-CREATE INDEX job_name ON job_suffix(job_name)
-;
+   process_id           varchar(40) PRIMARY KEY NOT NULL,
+   job_name             varchar(128) NOT NULL,
+   job_folder           varchar(128) NOT NULL,
+   job_status           varchar(20) ,
+   os_process_id        integer,
+   exit_code            integer,
+   duration 	        int default 0,
+   ins_timestamp        datetime DEFAULT current_timestamp(),
+   mod_timestamp        datetime DEFAULT current_timestamp()
+);
 
